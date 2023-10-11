abfb5779ab63f8be5202f3388460f59bc0fe508b
Phillip Cole <phillipcole123@gmail.com>
2023-10-11 00:59:42 -0500

    Added api key functionality loaded from .env

--- main.py
+++ main.py
@@ -6,29 +6,61 @@ from timezonefinder import TimezoneFinder
 from datetime import datetime
 import requests
 import pytz
+from dotenv import load_dotenv
+import os 
+
+# The function `configure()` loads the environment variables from a .env file.
+def configure():
+    load_dotenv()
 
 # The code is creating the main window for the weather app.
 root=Tk()
 root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
+# `root.after_idle(configure)` is scheduling the `configure()` function to be called after all pending
+# events have been processed by the main event loop. This ensures that the environment variables are
+# loaded from the .env file before the rest of the code is executed.
+root.after_idle(configure) 
 
 
-# The function `getWeather()` takes a city as input, uses geolocation to find the longitude and
-# latitude of the city, and then uses a timezone finder to determine the timezone of the city.
-
+# The function `getWeather()` retrieves weather information for a given city and displays it on a GUI.
 def getWeather():
-    city=textfield.get()
+    try:
+        city=textfield.get()
 
-    geolocator = Nominatim(user_agent="Weather App")
-    location = geolocator.geocode(city)
-    obj = TimezoneFinder()
-    result = obj.timezone_at(lng = location.longitude, lat = location.latitude)
-    home=pytz.timezone(result)
-    local_time=datetime.now(home)
-    current_time=local_time.strftime("%I:%M %p")
-    clock.config(text = current_time)
-    name.config(text = "CURRENT WEATHER")
+        geolocator = Nominatim(user_agent="Weather App")
+        location = geolocator.geocode(city)
+        obj = TimezoneFinder()
+        result = obj.timezone_at(lng = location.longitude, lat = location.latitude)
+        home=pytz.timezone(result)
+        local_time=datetime.now(home)
+        current_time=local_time.strftime("%I:%M %p")
+        clock.config(text = current_time)
+        name.config(text = "CURRENT WEATHER")
+
+
+        #weather
+        api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&&appid={os.getenv('api_key')}"
+        
+        json_data = requests.get(api).json()
+        condition = json_data['weather'][0]['main']
+        description = json_data['weather'][0]['description']
+        temp = int(json_data['main']['temp']-273.15)
+        pressure = json_data['main']['pressure']
+        humidity = json_data['main']['humidity']
+        wind = json_data['wind']['speed']
+
+        lab_temp.config(text = (temp, "°"))
+        lab_con.config(text = (condition, "|", "FEELS", "LIKE", temp, "°" ))
+
+        lab_wind.config(text = wind)
+        lab_hum.config(text = humidity)
+        lab_desc.config(text = description)
+        lab_pres.config(text = pressure)
+    
+    except Exception as e:
+        messagebox.showerror("Weather App", "Invalid Entry!")
 
 
 
@@ -81,22 +113,26 @@ lab_desc.place(x=430,y=400)
 lab_pres = Label(root,text="PRESSURE", font=("Helvetica", 15, 'bold'), fg="white",bg="#1ab5ef")
 lab_pres.place(x=650,y=400)
 
-t = Label(font=("arial",70,"bold"), fg="#ee666d")
-t.place(x=400,y=150)
-c = Label(font=("arial",15,"bold"))
-c.place(x=400, y=250)
+lab_temp = Label(font=("arial",70,"bold"), fg="#ee666d")
+lab_temp.place(x=400,y=150)
+lab_con = Label(font=("arial",15,"bold"))
+lab_con.place(x=400, y=250)
 
-w = Label(text="...",font=("arial",20,"bold"), bg="#1ab5ef")
-w.place(x=120,y=430)
+lab_wind = Label(text="...",font=("arial",20,"bold"), bg="#1ab5ef")
+lab_wind.place(x=120,y=430)
 
-w = Label(text="...",font=("arial",20,"bold"), bg="#1ab5ef")
-w.place(x=280,y=430)
+lab_hum = Label(text="...",font=("arial",20,"bold"), bg="#1ab5ef")
+lab_hum.place(x=280,y=430)
 
-w = Label(text="...",font=("arial",20,"bold"), bg="#1ab5ef")
-w.place(x=450,y=430)
+lab_desc = Label(text="...",font=("arial",20,"bold"), bg="#1ab5ef")
+lab_desc.place(x=450,y=430)
 
-w = Label(text="...",font=("arial",20,"bold"), bg="#1ab5ef")
-w.place(x=670,y=430)
+lab_pres = Label(text="...",font=("arial",20,"bold"), bg="#1ab5ef")
+lab_pres.place(x=670,y=430)
 
 
+# `root.mainloop()` is a method that starts the main event loop of the tkinter application. It
+# continuously listens for events such as button clicks, mouse movements, and keyboard inputs, and
+# updates the GUI accordingly. It keeps the application running until the user closes the window or
+# exits the program.
 root.mainloop()
